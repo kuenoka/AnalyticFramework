6c06de5e16f0876315061e194f8e8a6cb4809d63
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-10-16 16:02:40 -0500

    Initial Commit

---
+++ AnalyticFramework/AnalyticFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticFramework.h
+//  AnalyticFramework
+//
+//  Created by Kingsley Enoka on 10/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
