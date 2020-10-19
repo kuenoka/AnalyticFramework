5f8b7bc521097ea28ef01fa44d3928f33dd52017
Kingsley Eoka <kingsleyenoka@gmail.com>
2020-10-19 14:43:07 -0500

    first commit

---
+++ AnalyticFramework/Analytics.swift
@@ -0,0 +1,16 @@
+//
+//  Analytics.swift
+//  AnalyticFramework
+//
+//  Created by Kingsley Enoka on 10/16/20.
+//
+
+import Foundation
+
+public class Analytics {
+  public init() {}
+  
+  public func log(message: String) {
+    print("Log message: ", message)
+  }
+}
